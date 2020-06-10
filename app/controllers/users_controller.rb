11640b880ede3f0c94354cfc81f615d24619fc25
Brittany Blake <brittany.blake49@gmail.com>
2020-06-10 16:01:35 +0200

    Add edit to forms

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,23 @@ class UsersController < ApplicationController
         end
     end
 
+     def show
+         @user = User.find(params[:id])
+     end
+
+      def edit
+         @user = User.find(params[:id])
+     end
+     
+     def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            redirect_to @user
+        else
+            render 'edit'
+        end
+     end
+
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
